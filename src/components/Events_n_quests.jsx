8a9f56eab182d35aaddccf389fc52684cdee344a
rom <romula@gmail.com>
2017-09-11 00:23:59 +0300

    fix filter for events

--- src/components/Events_n_quests.jsx
+++ src/components/Events_n_quests.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Link, NavLink, Route } from 'react-router-dom';
+import { Link, NavLink, Route, Redirect } from 'react-router-dom';
 
 import Events_category from './Events_category';
 import Event from './Event';
@@ -8,31 +8,19 @@ import Event from './Event';
 import quests from './../data/quests';
 
 class Events_n_quests extends React.Component {
-    constructor(props){
-      super(props);
-      this.state = {
-        val: '',
-        openList: false,
-        category: []
-      }
-      this.changeValue = this.changeValue.bind(this);
-      this.openCategoryList = this.openCategoryList.bind(this);
-      this.closeCategoryList = this.closeCategoryList.bind(this);
-      this.changeCategory = this.changeCategory.bind(this);
-    }  
-  changeValue(){
-    
-  }
-  changeCategory(){    
-  }
+  constructor(props){
+    super(props);
+    this.state = {
+      val: '',
+      openList: false,
+      category: []
+    }
+    this.changeValue = this.changeValue.bind(this);
+    this.openCategoryList = this.openCategoryList.bind(this);
+    this.closeCategoryList = this.closeCategoryList.bind(this);
+    this.changeCategory = this.changeCategory.bind(this);
+  } 
   
-  openCategoryList(){
-    this.setState({openList: true});
-  }
-  closeCategoryList(){
-    this.setState({openList: false});
-    this.setState({val: this.props.match.params.id});
-  }
   componentDidMount(){
     var questsArr = Object.values(quests); 
     var newCat = {};
@@ -43,25 +31,56 @@ class Events_n_quests extends React.Component {
         categoryArr[i++] = questsArr[q];
       }
     }
+/***********************************************
+//Вариант с new Set выдает одномерный массив, что исключает возможность переделки линка
+    categoryArr = questsArr.map(item => {
+      return item.cat;
+    })
+    categoryArr = [... new Set(categoryArr)];
+************************************************/
+    categoryArr = categoryArr.map(item => {
+      return newCat = {
+       cat: item.cat,
+       link: item.link
+      }
+    })
     this.setState({category: categoryArr});
-    console.log(categoryArr);
+  }
+  changeValue(){}
+  
+  changeCategory(){
+    this.closeCategoryList();
   }
   
-    render(){     
+  openCategoryList(){
+    this.setState({openList: true});
+  }
+  closeCategoryList(){
+    this.setState({openList: false});
+//    this.setState({val: this.props.match.params.id});
+  }
+  
+    render(){
+      var redirect = '';
+      if(this.props.location.pathname == '/events_n_quests'){ 
+        redirect = <Redirect from='/events_n_quests' to='/events_n_quests/all' />;
+      }
       return (
             <section id="events_n_quests" className="screen-section"> 
                <h2 className="title--page">Задания и квесты</h2>
-               <div className="field-block">
+               <div className="field-block" >
                  <input type="text" placeholder="выберите категорию" value={this.state.val} onFocus={this.openCategoryList}/>
                  <ul className={this.state.openList == true ? 'field__dropdown category__dropdown open' : 'category__dropdown'}>
+                    <li><Link to={`/events_n_quests/all`} className="category__link" onClick={this.changeCategory} id="all">все</Link></li>
                    {this.state.category.map((item, val) => { 
                      return(
-                       <li key={val}><Link to={`/events_n_quests/${item.link}`} className="category__link" onClick={this.closeCategoryList} id={item.link}>{item.cat}</Link></li>
+                       <li key={val}><Link to={`/events_n_quests/${item.link}`} className="category__link" onClick={this.changeCategory} id={item.link}>{item.cat}</Link></li>
                      )
                    })}
                  </ul>              
                </div>
               <div>
+                {redirect}
                 <Route exact path={`/events_n_quests/:link`} component={Events_category}/>
                 <Route exact path={`/events_n_quests/:link/:id`} component={Event}/>
               </div> 
